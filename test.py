7911435bea61a418bd36f68279830321a943edae
Bjorn Goossens <b.goossens@bullit.digital>
2023-07-26 14:36:35 +0200

    List the list

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import os
+
+print("Listing current dir")
+print(os.listdir("./"))
+
+print("Listing dir above")
+print(os.listdir("./../"))
+
+print("Listing test-data dir")
+print(os.listdir("./../test-data"))
 
 df_towing_vehicles = pd.read_parquet("../test-data/data/towing_vehicles.parquet")
 print(df_towing_vehicles.head())
